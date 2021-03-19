c6b84c5328ebc896ba93af1ff3052cb26564f05d
Jimmy De Kroon <jimmydekroon@gmail.com>
2021-03-19 10:06:57 +0100

    Render & functionality

--- src/app.js
+++ src/app.js
@@ -1,4 +1,5 @@
 // Require third-party modules
+const { response } = require('express');
 const express = require('express');
 const fetch = require('node-fetch');
 
@@ -11,7 +12,9 @@ app.set('view engine', 'ejs');
 // Tell the views engine/ejs where the template files are stored (Settingname, value)
 app.set('views', 'views');
 
-// Create a home route
+let launchpadid
+let payloadid
+
 app.get('/', async function(req, res) {
 	const response = await fetch('https://api.spacexdata.com/v4/launches/next');
 	const data = await response.json();
@@ -19,7 +22,40 @@ app.get('/', async function(req, res) {
 	// console.log(data);
 
 	res.render('index', {
-		launchData: data
+		launchDataNext: data
+	});
+
+	launchpadid = data.launchpad
+	payloadid = data.payloads[0]
+});
+
+app.get('/details/:id', async function(req, res) {
+	
+	function fetchJSON(url) {
+		return fetch(url).then(response => response.json());
+	}
+
+	let urls = [
+		'https://api.spacexdata.com/v4/launches/' + req.params.id,
+		'https://api.spacexdata.com/v4/launchpads/' + launchpadid,
+		'https://api.spacexdata.com/v4/payloads/' + payloadid,
+		'https://api.spacexdata.com/v4/launches/next'
+	  ];
+	
+	let promises = urls.map(url => fetchJSON(url));
+	
+	Promise.all(promises).then(responses => {
+		var launchDataId = responses[0];
+    	var launchpadData = responses[1];
+    	var payloadData = responses[2];
+    	var launchDataNext = responses[3];
+
+		res.render('details', {
+			launchDataId: launchDataId,
+			launchpadData: launchpadData,
+			payloadData: payloadData,
+			launchDataNext: launchDataNext
+		})
 	});
 });
 
